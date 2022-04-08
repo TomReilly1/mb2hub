3f4079b24b32598367066d1c7e614a52fc2154cd
TomReilly1 <reilly.thomas.c@gmail.com>
2022-04-08 09:28:06 -0400

    change 'mb2' to 'armor' in file name for better desc.

---
+++ tools/armor_reduce_json.py
@@ -0,0 +1,68 @@
+import os, xmltodict, json
+from pathlib import Path
+
+
+def reduceJSON(prefix, file):
+	output_json = []
+	file_path = prefix / file
+
+	with open(file_path) as f:
+		data = json.load(f)
+		list_of_AR_obj = data['Items']['Item']
+
+		for i in list_of_AR_obj:
+			curr_obj = {}
+
+			curr_obj['id'] = i['@id']
+			curr_obj['name'] = i['@name']
+			curr_obj['culture'] = i['@culture'].split('.')[1]
+			curr_obj['weight'] = i['@weight']
+			curr_obj['type'] = i['@Type']
+			curr_obj['armor'] = {}
+
+			try:
+				curr_obj['armor']['head'] = i['ItemComponent']['Armor']['@head_armor']
+			except:
+				curr_obj['armor']['head'] = "0"
+
+			try:
+				curr_obj['armor']['body'] = i['ItemComponent']['Armor']['@body_armor']
+			except:
+				curr_obj['armor']['body'] = "0"
+			
+			try:
+				curr_obj['armor']['arm'] = i['ItemComponent']['Armor']['@arm_armor']
+			except:
+				curr_obj['armor']['arm'] = "0"
+			
+			try:
+				curr_obj['armor']['leg'] = i['ItemComponent']['Armor']['@leg_armor']
+			except:
+				curr_obj['armor']['leg'] = "0"
+
+
+			output_json.append(curr_obj)
+
+
+	new_json_name = file.split('.')[0] + "_reduced.json"
+	with open(new_json_name, "w") as final:
+		json.dump(output_json, final)
+	# json.dumps(output_json, indent=4)
+
+
+
+if __name__ == "__main__":
+	# Get current working directory
+	cwd = os.getcwd()
+
+	# CHANGE BELOW TO THE CORRECT VERSION (no spaces)
+	VERSION = '1.7.2beta'
+
+
+	prefix = Path(f"{cwd}/{VERSION}/json/")
+	
+	reduceJSON(prefix, "head_armors.json")
+	reduceJSON(prefix, "shoulder_armors.json")
+	reduceJSON(prefix, "body_armors.json")
+	reduceJSON(prefix, "arm_armors.json")
+	reduceJSON(prefix, "leg_armors.json")
---
+++ tools/armor_xml_to_json.py
@@ -0,0 +1,37 @@
+import os, xmltodict, json
+from pathlib import Path
+
+
+def convertToJSON(prefix, file):
+	file_path = prefix / file
+
+	with open(file_path) as xml_file:
+		
+		data_dict = xmltodict.parse(xml_file.read())
+		xml_file.close()
+		
+
+		json_data = json.dumps(data_dict)
+		
+		name = file.split(".")[0]
+		with open(name + ".json", "w") as json_file:
+			json_file.write(json_data)
+			json_file.close()
+
+
+if __name__ == "__main__":
+	# Get current working directory
+	cwd = os.getcwd()
+
+	# CHANGE BELOW TO THE CORRECT VERSION (no spaces)
+	VERSION = '1.7.2beta'
+	
+	
+	prefix = Path(f"{cwd}/{VERSION}/modified/")
+
+	convertToJSON(prefix, "head_armors.xml")
+	convertToJSON(prefix, "shoulder_armors.xml")
+	convertToJSON(prefix, "body_armors.xml")
+	convertToJSON(prefix, "arm_armors.xml")
+	convertToJSON(prefix, "leg_armors.xml")
+
