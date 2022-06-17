0e46baadde295ee82afacd14e4227ae0158e5893
TomReilly1 <reilly.thomas.c@gmail.com>
2022-06-17 19:48:48 -0400

    changed syle for card

--- website/frontend/src/components/CulturesCard.vue
+++ website/frontend/src/components/CulturesCard.vue
@@ -1,48 +1,60 @@
 <script setup>
-import { ref, onMounted, onUpdated } from "vue";
 import { useRoute } from 'vue-router';
 
-console.log('made it to culture card component');
 
 const route = useRoute();
 const props = defineProps({cultureObj: Object});
-
-
 </script>
 <!------------------------------------------------------->
 <template>
     <section>
         <div v-if="cultureObj !== null && cultureObj !== undefined" class="card-desc">
-            <h2>{{cultureObj.name || 'Nothing passed yet'}}</h2>
-            <p v-if="cultureObj.is_main_culture">The {{cultureObj.name}} culture is a main culture of Calradia</p>
-            <p v-else>The {{cultureObj.name}} culture is a minor culture of Calradia</p>
-            <h3>Description</h3>
-            <p>{{cultureObj.desc_text}}</p>
+            <div>
+                <h2>{{cultureObj.name || 'Nothing passed yet'}}</h2>
+                <span v-if="cultureObj.is_main_culture">The {{cultureObj.name}} culture is a main culture of Calradia.</span>
+                <span v-else>The {{cultureObj.name}} culture is a minor culture of Calradia</span>
+            </div>
+            <hr />
+            <div>
+                <h3>History</h3>
+                <p v-if="cultureObj.is_main_culture">{{cultureObj.desc_text}}</p>
+                <span v-else>N/A</span>
+            </div>
         </div>
     </section>
 </template>
 <!------------------------------------------------------->
 <style scoped>
-    h2 {
-        text-transform: capitalize;
-        font-size: 3rem;
-        margin: 0;
-        border: 0;
-        padding: 0 0 20px;
-    }
+h2 {
+    text-transform: capitalize;
+    font-size: 3rem;
+    font-weight: 700;
+    margin: 0 0 20px;
+    border: 0;
+    padding: 0;
+}
 
-    div {
-        max-width: 40rem;
-        margin: 0 auto 3rem;
-        padding: 2rem;
-        border-radius: 1rem;
-        background-color: var(--bluegray-800);
-        color: var(--yellow-300);
-        box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-    }
+.card-desc {
+    max-width: 40rem;
+    margin: 0 auto 3rem;
+    padding: 40px;
+    border-radius: 5%;
+    background-color: var(--bluegray-900);
+    color: var(--yellow-300);
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+}
 
-    div > p{
-        color: var(--bluegray-100);
-        font-size: 1.2rem;
-    }
+.card-desc > div {
+    margin: 0 0 30px;
+}
+
+span {
+    color: var(--bluegray-100);
+    font-size: 1.5rem;
+}
+
+p{
+    color: var(--bluegray-100);
+    font-size: 1.2rem;
+}
 </style>
