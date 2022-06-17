526dad9844dfa87958311fbea10d636bab903812
TomReilly1 <reilly.thomas.c@gmail.com>
2022-06-17 19:48:03 -0400

    minor style changes

--- website/frontend/src/App.vue
+++ website/frontend/src/App.vue
@@ -11,7 +11,7 @@ import GlobalFooter from "@/components/GlobalFooter";
 <!--------------------------------------------------------------------->
 <style>
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
+    font-family: 'Open Sans', 'Poppins', 'Merriweather Sans', Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
@@ -27,7 +27,7 @@ import GlobalFooter from "@/components/GlobalFooter";
     padding-bottom: 10rem;
 }
 
-nav {
+/* nav {
     padding: 30px;
 }
 
@@ -39,16 +39,16 @@ nav a {
 
 nav a.router-link-exact-active {
     color: var(--yellow-600);
-}
+} */
 
 h1 {
-    color: var(--bluegray-700);
+    color: var(--bluegray-900);
     font-size: 4.5rem;
     font-family: Georgia, 'Times New Roman', Times, serif;
     text-decoration: underline;
     margin: 0;
     border: 0;
     padding: 2rem 0 2.5rem;
-    text-shadow: 0px 0px 10px aliceblue;
+    text-shadow: 0px 0px 4px aliceblue;
 }
 </style>
