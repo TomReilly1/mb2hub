90da2bf0406ea0ca11c9635baebc33fe49bd1f36
TomReilly1 <reilly.thomas.c@gmail.com>
2022-06-17 20:09:29 -0400

    fixed armors card route

--- website/backend/routers/routes.js
+++ website/backend/routers/routes.js
@@ -34,9 +34,9 @@ router.get('/armors', (req,res) => {
 });
 
 router.get('/armors/:id', (req,res) => {
-    console.log(`SELECT * FROM kingdoms WHERE id = ${req.params['id']};`);
+    console.log(`SELECT * FROM armors WHERE id = ${req.params['id']};`);
 
-    db.one(`SELECT * FROM kingdoms WHERE id = '${req.params['id']}';`)
+    db.one(`SELECT * FROM armors WHERE id = '${req.params['id']}';`)
     .then(rows => {
         console.log(rows);
         res.json(rows);
