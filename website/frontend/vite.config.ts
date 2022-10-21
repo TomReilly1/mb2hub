dff418e59b8ce68c33f5f315dd5038196ae6e7e1
TomReilly1 <reilly.thomas.c@gmail.com>
2022-10-20 21:29:06 -0400

    add happy-dom for vitest config

--- website/frontend/vite.config.ts
+++ website/frontend/vite.config.ts
@@ -1,11 +1,14 @@
 import { fileURLToPath, URL } from 'url'
 
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
+    test: {
+        environment: 'happy-dom'
+    },
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
