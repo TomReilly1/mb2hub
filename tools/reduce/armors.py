8c9f67efd3ffb55820921432e53db31f038eeb8f
TomReilly1 <reilly.thomas.c@gmail.com>
2023-02-12 12:14:20 -0500

    added nulls to armor ratings for armor types that cannot have a value for specific armor coverage

--- tools/reduce/armors.py
+++ tools/reduce/armors.py
@@ -16,7 +16,47 @@ R_PATH_ARM = f'{PROJ_DIR}/{VERSION}/json/arm_armors.json'
 R_PATH_LEG = f'{PROJ_DIR}/{VERSION}/json/leg_armors.json'
 
 
-def add_armor_type(read_path):
+def handle_armor_rating_values(armor_type: str, prefix: dict) -> dict:
+    armor_val_dict = {
+        'head_armor': None,
+        'body_armor': None,
+        'arm_armor': None,
+        'leg_armor': None
+    }
+
+    match armor_type:
+        case 'HeadArmor':
+            armor_val_dict['head_armor'] = (prefix.get('@head_armor')
+                                            if prefix.get('@head_armor') else 0)
+        case 'ShoulderArmor':
+            armor_val_dict['body_armor'] = (prefix.get('@body_armor')
+                                            if prefix.get('@body_armor') else 0)
+            armor_val_dict['arm_armor'] = (prefix.get('@arm_armor')
+                                            if prefix.get('@arm_armor') else 0)
+        case 'BodyArmor':
+            armor_val_dict['body_armor'] = (prefix.get('@body_armor')
+                                            if prefix.get('@body_armor') else 0)
+            armor_val_dict['arm_armor'] = (prefix.get('@arm_armor')
+                                            if prefix.get('@arm_armor') else 0)
+            armor_val_dict['leg_armor'] = (prefix.get('@leg_armor')
+                                            if prefix.get('@leg_armor') else 0)
+        case 'ArmArmor':
+            armor_val_dict['arm_armor'] = (prefix.get('@arm_armor')
+                                            if prefix.get('@arm_armor') else 0)
+        case 'LegArmor':
+            armor_val_dict['leg_armor'] = (prefix.get('@leg_armor')
+                                            if prefix.get('@leg_armor') else 0)
+        case _:
+            raise Exception('unkown armor type')
+
+    for key, val in armor_val_dict.items():
+        if val:
+            armor_val_dict[key] = int(val)
+
+    return armor_val_dict
+
+
+def add_armor_type(read_path: str) -> list:
     json_arr = read_json(read_path)['Items']['Item']
     output_array = []
 
@@ -34,29 +74,17 @@ def add_armor_type(read_path):
         else:
             curr_obj['type'] = i['@Type']
 
-        try:
-            curr_obj['head_armor'] = i['ItemComponent']['Armor']['@head_armor']
-        except KeyError:
-            curr_obj['head_armor'] = "0"
-        try:
-            curr_obj['body_armor'] = i['ItemComponent']['Armor']['@body_armor']
-        except KeyError:
-            curr_obj['body_armor'] = "0"
-        try:
-            curr_obj['arm_armor'] = i['ItemComponent']['Armor']['@arm_armor']
-        except KeyError:
-            curr_obj['arm_armor'] = "0"
-        try:
-            curr_obj['leg_armor'] = i['ItemComponent']['Armor']['@leg_armor']
-        except KeyError:
-            curr_obj['leg_armor'] = "0"
+        armor_prefix = i['ItemComponent']['Armor']
+        armor_vals_dict = handle_armor_rating_values(curr_obj['type'], armor_prefix)
+        for key, val in armor_vals_dict.items():
+            curr_obj[key] = val
 
         output_array.append(curr_obj)
 
     return output_array
 
 
-def reduce_armors():
+def reduce_armors() -> None:
     final_array = add_armor_type(R_PATH_HEAD)
     final_array += add_armor_type(R_PATH_SHOULDER)
     final_array += add_armor_type(R_PATH_BODY)
